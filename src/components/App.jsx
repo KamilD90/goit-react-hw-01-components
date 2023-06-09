7040e2d24b4ea476320c47ee39362d05e3269e86
KamilD90 <117952413+KamilD90@users.noreply.github.com>
2023-06-09 21:21:29 +0200

    work start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import Profile from './Profile';
+import user from './user';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +10,13 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
+      <Profile {...user} />
       React homework template
     </div>
   );
 };
+
+export default App;
